618b0653c90813df584e9625ef3b521c13419336
Matheus de Souza <msouza@softexpert.com>
2023-12-04 20:14:28 -0300

    commit inicial

---
+++ login.js
@@ -0,0 +1,17 @@
+document.getElementById('login-button').addEventListener('click', function() {
+    loginWithSpotify();
+});
+
+function loginWithSpotify() {
+    const clientId = 'ae3422e92ee84fc39e64181f8c17cb7f'; // Replace with your client ID
+    const redirectUri = encodeURIComponent('https://localhost:8080'); // Replace with your redirect URI
+    const scopes = [
+        'playlist-modify-public',
+        'user-follow-read',
+        'user-top-read',
+        'user-library-read'
+    ];
+    const url = `https://accounts.spotify.com/authorize?response_type=code&client_id=${clientId}&scope=${encodeURIComponent(scopes.join(' '))}&redirect_uri=${redirectUri}`;
+
+    window.location.href = url;
+}
